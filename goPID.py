b1442f6881e90a397cfbd081a3610524cb62b0c2
Russell Castro <201190804@student.lbschools.net>
2022-08-02 13:44:09 -0700

    addedpathing

--- goPID.py
+++ goPID.py
@@ -6,9 +6,9 @@ drone.connect()
 
 dt = 0.1 #update period
 
-x_pos = 0
-y_pos = 0
-z_pos = 0
+x_pos = 0.0
+y_pos = 0.0
+z_pos = 0.0
 
 xkP = 0.0
 xkI = 0.0
@@ -27,19 +27,21 @@ yI = 0.0
 zI = 0.0
 
 def reg_update():
-    x_accel = drone.get_acceleration_x
-    y_accel = drone.get_acceleration_y
-    z_accel = drone.get_acceleration_z
+    x_accel = drone.get_acceleration_x()
+    y_accel = drone.get_acceleration_y()
 
     x_vel = x_accel * dt
     y_vel = y_accel * dt
-    z_vel = z_accel * dt
+
+    global x_pos
+    global y_pos
+    global z_pos
 
     x_pos += 0.5 * x_accel * dt ** 2
     y_pos += 0.5 * y_accel * dt ** 2
-    z_pos += 0.5 * z_accel * dt ** 2
+    z_pos = drone.get_barometer() #barometer units
 
-def go_PID(x_set, y_set, z_set):
+def doPID(x_set, y_set, z_set):
     x_error = x_set - x_pos
     xI += xkI * x_error * dt
 
@@ -57,15 +59,21 @@ def go_PID(x_set, y_set, z_set):
     z_error = z_set - z_pos
     xI += xkI * x_error * dt
 
-    z_u = zkP * z_error #x effort
+    z_u = zkP * z_error #z effort
     + xI
     + zkD * z_error / dt
 
     drone.send_rc_control(round(x_u), round(y_u), round(z_u))
     
-def main():
+def goPID(x_set, y_set, z_set, x_tol, y_tol, z_tol):
     keep_going = True
 
     while keep_going == True:
         reg_update()
+        doPID(x_set, y_set, z_set)
         time.sleep(dt)
+
+        if (x_set - x_set * x_tol < x_pos > x_set + x_set * x_tol
+        & y_set - y_set * y_tol < y_pos > y_set + y_set * y_tol
+        & z_set - z_set * z_tol < z_pos > z_set + z_set * z_tol):
+            keep_going = False
